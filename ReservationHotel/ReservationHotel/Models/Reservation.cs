39dff518229b4c69260fe7bc92f30a109018d777
VincentHuynh03 <vincentgamer2213@outlook.com>
2023-10-23 14:44:03 -0400

    Fix des 2 commits précédentes ( Classe modèles et Validations)

---
+++ ReservationHotel/ReservationHotel/Models/Reservation.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReservationHotel.Models
+{
+    public class Reservation
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ReservationId { get; set; }
+
+        [Required(ErrorMessage = "La date de début de la chambre est obligatoire.")]
+        [Column(TypeName = "DateTime")]
+        [Display(Name = "Date de début")]
+        public DateTime? DateDebut { get; set; }
+
+
+        [Required(ErrorMessage = "La date de fin de la chambre est obligatoire.")]
+        [Column(TypeName = "DateTime")]
+        [Compare(nameof(DateDebut), ErrorMessage = "La date de fin ne peut pas être avant la date de début.")]
+        [Display(Name = "Date de fin")]
+        public DateTime? DateFin { get; set; }
+
+        [Required]
+        public bool? Annuler { get; set; } = false;
+
+
+
+
+
+
+
+
+
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Utilisateur? Utilisateur { get; set; }
+
+        public int ChambreId { get; set; }
+        [ForeignKey("ChambreId")]
+        public virtual Chambre? Chambre { get; set; }
+
+
+
+
+
+    }
+}
